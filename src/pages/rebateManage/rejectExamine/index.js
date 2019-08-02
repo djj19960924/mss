fa1758b03b98ba372904d4c60eb9b82a0fe35549
zjw <626829146@qq.com>
2019-08-02 15:24:50 +0800

    驳回小票 去掉国家商场限制

--- src/pages/rebateManage/rejectExamine/index.js
+++ src/pages/rebateManage/rejectExamine/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import {Select, Table, message, Pagination, Button, Icon, Modal} from 'antd';
-import countryList from '@js/country/';
 import moment from 'moment';
 import './index.less';
-
-const Option = Select.Option;
 
 class rejectExamine extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      //选择的国家
-      country: '',
-      //国家列表
-      countries: [],
-      //选择的商场
-      shop: undefined,
-      // 商场列表
-      shopList: [],
       pageNum: 1,
       pageSize: 100,
       pageSizeOptions: ['50', '100', '200', '300'],
@@ -25,47 +14,15 @@ class rejectExamine extends React.Component{
       pageTotal: 0,
       //table数据
       dataSource: [],
-      //商品名
-      mallName: undefined,
     };
   }
   componentDidMount() {
-    let countries = [];
-    for (let i of countryList) countries.push(<Option key={i.id} value={i.nationName}>{i.nationName}</Option>);
-    this.setState({countries: countries});
-
-  }
-  // 监听选择国家事件
-  selectCountry(val) {
-    this.setState({
-      country: val,
-      dataSource: [],
-      pageTotal: 0,
-      mallName: undefined
-    });
-    this.ajax.post('/mall/getMallListByNationName',{nationName:val}).then(r => {
-      if (r.data.status === 10000) {
-        const { data } = r.data;
-        let dataList = [];
-        for (let i of data) dataList.push(<Option key={i.mallId} value={i.mallName}>{i.mallName}</Option>);
-        this.setState({shopList: dataList});
-      }
-      r.showError();
-    }).catch(r => {
-      this.ajax.isReturnLogin(r, this);
-    });
-  }
-  // 监听选择商店事件
-  selectShop(val) {
-    this.setState({mallName: val}, () => {
-      this.rejectByMall(val);
-    });
+    this.rejectByMall();
   }
   //根据商场获取驳回小票
   rejectByMall() {
-    const {mallName, pageNum, pageSize} = this.state;
+    const {pageNum, pageSize} = this.state;
     this.ajax.post('/recipt/getReciptOfRejected', {
-      mallName: mallName,
       pageNum: pageNum,
       pageSize: pageSize
     }).then(r => {
@@ -136,7 +93,7 @@ class rejectExamine extends React.Component{
                 )))
           }</div>
         ),},
-      {title: '操作', dataIndex: 'pictureUrl', key: 'pictureUrl', width: 80,
+      {title: '操作', dataIndex: 'pictureUrl', key: 'pictureUrl', width: 100,
         render: (text, record) => (  //塞入内容
           <div className="ellipsis">
             <Button type="primary"
@@ -145,25 +102,12 @@ class rejectExamine extends React.Component{
             >查看</Button>
           </div>
         ),}];
-    const {countries, shopList, country, mallName, pageTotal, pageSize, pageSizeOptions, pageNum, dataSource} = this.state;
+    const {country, mallName, pageTotal, pageSize, pageSizeOptions, pageNum, dataSource} = this.state;
     return (
       <div className="rejectExamine contentMain">
         <div className="title">
           <div className="titleMain">已驳回小票</div>
           <div className="titleLine" />
-        </div>
-        <div className="shopSelect">
-          <span>所属国家: </span>
-          <Select className="selectShops"
-                  placeholder="请选择国家"
-                  onChange={this.selectCountry.bind(this)}
-          >{countries}</Select>
-          <span style={{marginLeft: 20}}>所属商场: </span>
-          <Select className="selectShops"
-                  placeholder="请选择商场"
-                  value={this.state.mallName}
-                  onChange={this.selectShop.bind(this)}
-          >{shopList}</Select>
         </div>
         <div className="tableMain">
           <Table id="table"
@@ -172,21 +116,8 @@ class rejectExamine extends React.Component{
                  dataSource={dataSource}
                  bordered
                  rowKey={(record, index) => `${index}`}
+                 scroll={{ y: 550, x: 800 }}
                  pagination={false}
-                 locale={{
-                   emptyText: <div className="noShop">
-                     {!country &&
-                       <div className="noShopDiv">
-                         <Icon type="shop" className="iconShop"/><span>请选择国家</span>
-                       </div>
-                     }
-                     {country && !mallName &&
-                       <div className="noShopDiv">
-                         <Icon type="shop" className="iconShop"/><span>请选择商场</span>
-                       </div>
-                     }
-                   </div>,
-                 }}
           />
           <Pagination className="tablePagination"
                       total={pageTotal}
